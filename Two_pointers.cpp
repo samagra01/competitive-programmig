65f9b0d56b3fb9acb70f87bdab1669941b3c3198
samagra pathak <88074574+samagra01@users.noreply.github.com>
2022-06-16 15:35:14 +0530

    Create Two_pointers.cpp

---
+++ Two_pointers.cpp
@@ -0,0 +1,48 @@
+  // DS for two pointers condition checking //  <<L< THIS METHOD IF FOR ZERO BASED INDEXING >>>
+   ll zero_ct = 0;
+   // pointers
+   ll head = -1,tail = 0;
+   ll ans = 0;
+   while(tail<n){
+    // while there is a next element to take and we will not voilate any condition//
+        while(head+1<n && (v[head+1]==1 || (v[head+1]==0 && zero_ct<k))){
+              head++;
+              if(v[head]==0)zero_ct++;
+        }
+        // update the ans //
+        ans = max(ans,head-tail+1);
+        // move tail ahead //
+        if(head>=tail){
+            if(v[tail]==0) zero_ct--;
+            tail++;
+        }else{
+            tail++;
+            head = tail -1;
+        }
+   }
+   cout << ans << endl;
+// without comments
+
+   ll zero_ct = 0;
+   
+   ll head = -1,tail = 0;
+   ll ans = 0;
+   while(tail<n){
+  
+        while(head+1<n && (v[head+1]==1 || (v[head+1]==0 && zero_ct<k))){
+              head++;
+              if(v[head]==0)zero_ct++;
+        }
+       
+        ans = max(ans,head-tail+1);
+       
+        if(head>=tail){
+            if(v[tail]==0) zero_ct--;
+            tail++;
+        }else{
+            tail++;
+            head = tail -1;
+        }
+   }
+   cout << ans << endl;
+
