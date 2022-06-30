647cd87a8c0394aa96cfcf56aac30711e24e1a33
samagra pathak <88074574+samagra01@users.noreply.github.com>
2022-06-30 15:15:06 +0530

    Update seive_template.cpp

--- seive_template.cpp
+++ seive_template.cpp
@@ -1,4 +1,8 @@
 
+const int MAX_SIZE = 2800001;vector<int>isprime(MAX_SIZE , true);vector<int> idx(MAX_SIZE);vector<int> prime;vector<int>SPF(MAX_SIZE);
+void manipulated_seive(int N) {  isprime[0] = isprime[1] = false ; for (int i = 2; i < N ; i++) {  if (isprime[i]) { prime.push_back(i);     SPF[i] = i;  }  for (int j = 0; j < (int)prime.size() && i * prime[j] < N && prime[j] <= SPF[i]; j++) {  isprime[i * prime[j]] = false;   SPF[i * prime[j]] = prime[j] ; }  }   for (int i = 0; i < (int)prime.size(); i++) { idx[prime[i]] = i + 1; }}
+vector<ll> primeFactors(ll n) { vector<ll> factors; while (n > 1){ factors.push_back(SPF[n]); n /= SPF[n]; } return factors;}
+ // complete seive in short is above //                            
 const int MAX_SIZE = 2800001;
 vector<int>isprime(MAX_SIZE , true);
 vector<int> idx(MAX_SIZE);
@@ -24,6 +28,15 @@ void manipulated_seive(int N) {
 // prime factors in log n using seive // up 1e6
 vector<int> primeFactors(int n) {
     vector<int> factors;
+    while (n > 1) {
+        factors.push_back(SPF[n]);
+        n /= SPF[n];
+    }
+    return factors;
+}
+// same with ll above uses int // better to use above one 
+vector<ll> primeFactors(ll n) {
+    vector<ll> factors;
     while (n > 1) {
         factors.push_back(SPF[n]);
         n /= SPF[n];
