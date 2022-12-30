1d5022b8fda124674b42b2a5509c3b15c4dd4a31
samagra pathak <88074574+samagra01@users.noreply.github.com>
2022-12-30 18:08:31 +0530

    Update factorial_precompute.cpp

--- factorial_precompute.cpp
+++ factorial_precompute.cpp
@@ -1,14 +1,40 @@
-const int N = 1e5 + 10;
+typedef long long int ll;
+
+const int MOD = 1e9 + 7;
+const int N = 200000 + 10;
 ll fact[N];
-ll facti[N];
-
+ll ifact[N];
+ll binpo(ll a, ll b) {
+	if (b == 0) return 1;
+	ll res = 1;
+	while (b) {
+		if (b & 1) {
+			res *= a;
+			res %= MOD;
+		}
+		a *= a;
+		a %= MOD;
+		b >>= 1;
+	}
+	return res;
+}
 void pre() {
-    fact[0] = fact[1] = 1;
-    facti[0] = facti[1] = binpomo(fact[0], MOD - 2, MOD);
+	fact[0] = fact[1] = 1;
+	ifact[0] = ifact[1] = binpo(1, MOD - 2);
+	for (int i = 2; i <= 200000; i++) {
+		fact[i] = i * fact[i - 1]; fact[i] %= MOD;
+		ifact[i] = binpo(fact[i], MOD - 2);
+	}
 
-    fo(i, 2, 1e5) {
-        fact[i] = i * fact[i - 1];
-        fact[i] %= MOD;
-        facti[i] = binpomo(fact[i], MOD - 2, MOD);
-    }
+}
+ll NCR(ll i, ll j) {
+	if (i < j) {
+		return 0;
+	}
+	if (j == 0)return 1;
+	ll ans = 1;
+	ans *= fact[i]; ans %= MOD;
+	ans *= ifact[j]; ans %= MOD;
+	ans *= ifact[i - j]; ans %= MOD;
+	return ans;
 }
